17030273f2adac40cf8e5abb81d596d6f8e43c47
dvlpr2003 <gayathrigaya698@gmail.com>
2024-06-17 19:53:04 +0530

    header

---
+++ src/ui/Home.jsx
@@ -0,0 +1,15 @@
+const Home = ()=>{
+    return(
+        <>
+        <header 
+        className="header flex item-center justify-content ">
+            <h4>Fast Pizza co.</h4>
+            <form>
+                <input type="text"  placeholder="Search by id " className="input-border pad-t-b pad-r-l transitions radius"/>
+            </form>
+        </header>
+        </>
+    )
+
+}
+export default Home;
